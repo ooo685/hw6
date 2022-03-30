8d66b9b92c6708ed9df7791c05add35ac234f725
ooo685 <olanike.o@utexas.edu>
2022-03-29 20:47:11 -0500

    finished code

---
+++ app.py
@@ -0,0 +1,38 @@
+from flask import Flask, request, jsonify 
+#from flask_cors import CORS
+#import requests as request
+app = Flask(__name__, static_folder='./build/', static_url_path='/')
+
+#CORS(app)
+
+db = {"Olanike": "Oyedele"}
+finame = {"fname": "null"}
+
+@app.route('/')
+def index():
+    return app.send_static_file('index.html')
+
+@app.route("/name", methods=["GET", "POST"])
+def name():
+    #get request
+    if request.method == "GET":
+        #get name from query param
+        #fname = request.args.get("fname") wasn't working for some reason
+        fname = finame["fname"]
+        #print(fname)
+        #if name is in db, return last name
+        if fname in db:
+            return jsonify(lname=db[fname])
+        else:
+            return jsonify(lname="User Not Found")
+    #post request
+    elif request.method == "POST":
+        #get name from json body
+        data = request.json
+        #print(data)
+        #set fname and lname in db
+        finame["fname"] = data
+        return "OK"
+
+if __name__ == 'main':
+    app.run()
--- src/App.js
+++ src/App.js
@@ -1,22 +1,71 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
+class NameForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      fname: '',
+      lname: ''
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  
+ 
+
+  handleChange(event) {
+    this.setState({fname: event.target.value});
+    //console.log(this.state.fname);
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    
+    fetch('/name', {
+      method:"POST",
+      cache: "no-cache",
+      headers:{
+        "content_type":"application/json",
+      },
+      body:JSON.stringify(this.state.fname)
+      }
+    ).then(response => {
+      fetch('/name', {
+       method:"GET"}
+     )
+       .then(response => response.json())
+       .then(data => this.setState({lname:data.lname}))
+   });
+       
+
+    //console.log(this.state.lname);
+  
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Name:
+          <input type="text" value={this.state.fname} onChange={this.handleChange} />
+        </label>
+        <input type="submit" value="Submit" />
+        <h1>Response From Server</h1>
+        <textarea value={this.state.lname} placeholder="Response"/>
+      </form>
+    );
+  }
+}
+
 function App() {
-  return (
+  
+  return(
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Request to Server</h1>
+        <NameForm/>
       </header>
     </div>
   );
